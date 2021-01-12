ca10eddbdc5336a4a3ddcc986539e5c77e7da49d
Thomas Levy <40259992+tlevy77@users.noreply.github.com>
2021-01-11 20:15:15 -0800

    Delete etlquery.sql

--- etlquery.sql
+++
@@ -1,9 +0,0 @@
-SELECT species.comname, observations.sciname, observations.lat, observations.lng, observations.subid
-FROM observations
-INNER JOIN species
-ON observations.comname = species.comname;
-
-SELECT songs.en, songtest.sci_name, songtest.latitude, songtest.longitude, songs.len, songs.d, songtest.songtype, songtest.elevation, songtest.bitrate
-FROM songs
-INNER JOIN songtest
-ON songs.id = songtest.xc_id;
